a6f74c0452061f458b76b4b845a018d6bb2d4e7a
rakeshgn31 <rakesh.gopinath31@gmail.com>
2016-11-26 12:05:32 +0200

    Initial Version Android Frontend

---
+++ app/src/main/java/junction/senseit/TasksListerActivity.java
@@ -0,0 +1,16 @@
+package junction.senseit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TasksListerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks_lister);
+    }
+
+
+}
